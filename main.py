e4b8c0c1ac6d53b15018c6a5d8fac595f4bba7ef
AndersOpoien <166613962+AndersOpoien@users.noreply.github.com>
2024-05-03 10:56:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,19 @@ c.execute('''
 
 conn.commit()
 
+#Lager en for postens database
+c.execute('''
+        CREATE TABLE IF NOT EXISTS postnummer (
+            Postnummer PRIMARY KEY,
+            Poststed,
+            Kommunenummer,
+            Kommunenavn, 
+            Kategori
+        )
+    ''')
+
+conn.commit()
+
 #Lager en funksjon for å lage en helt ny clean database. 
 def FunkLagDatabase(): 
     conn = sqlite3.connect('TomDatabase.db')
@@ -35,20 +48,73 @@ def FunkLagDatabase():
 def FunkImporterCSV():
     conn = sqlite3.connect('Brukerdatabase.db') 
     c = conn.cursor()
+    try:
+        with open('Brukerdatabase.csv', 'r') as csvfil: #Åpner opp brukerdatabase filen
+            lescsv = csv.reader(csvfil)  
+            next(lescsv) 
+
+            for row in lescsv: 
+
+                c.execute('''
+                    INSERT INTO brukere(
+                          fname, 
+                          ename, 
+                          epost, 
+                          tlf, 
+                          postnummer
+                          )
+                    VALUES(?, ?, ?, ?, ?)''', row)
+
+            conn.commit() 
+            #Her joiner jeg sammen brukere tabellen og postnummer tabellen.
+            #Her velger jeg hva jeg skal ha med, jeg tar med alt men utelukker  postnummer.Postnummer for å ikke få duplicates.
+            c.execute('''
+                SELECT
+                    brukere.fname,
+                    brukere.ename,
+                    brukere.epost,
+                    brukere.tlf,
+                    brukere.postnummer,
+                    postnummer.Poststed,
+                    postnummer.Kommunenummer,
+                    postnummer.Kommunenavn,
+                    postnummer.Kategori
+                FROM brukere 
+                JOIN postnummer ON postnummer.Postnummer = brukere.postnummer
+                ''') #Joiner brukere tabellen med postnummer tabellen hvis bruker.postnummer har samme verdi som postnummer.Postnummer kolonnen. 
+            
+            resultat = c.fetchall() #Lagrer resultatet i en variabel.
+
+            for result in resultat:
+                print(result) #Printer ut resultatet. 
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+#Importerer  postnummer
+def FunkImporterPostCSV():
+    conn = sqlite3.connect('Brukerdatabase.db') 
+    c = conn.cursor()
 #Try except
     try:
-        with open('Brukerdatabase.csv', 'r') as file: #Åpner opp brukerdatabase filen
-            reader = csv.reader(file)  #leser filen
-            next(reader) #Hopper over en linje
+        with open('Postnummerregister-Excel.csv', 'r') as csvfil: #Åpner opp postnummer filen
+            lescsv = csv.reader(csvfil)  #leser filen
+            next(lescsv) #Hopper over en linje
 
-            for row in reader: #For hver rad i csv filen legger den inn informasjon i brukere tabellen i databasen. 
+            for row in lescsv: #For hver rad i csv filen legger den inn informasjon i postnummer tabellen i databasen. 
                 c.execute('''
-                    INSERT INTO brukere(fname, ename, epost, tlf, postnummer)
+                    INSERT INTO postnummer(
+                          Postnummer,
+                          Poststed,
+                          Kommunenummer,
+                          Kommunenavn,
+                          Kategori
+                          )
                     VALUES(?, ?, ?, ?, ?)''', row) 
 
             conn.commit() #Committer sånn at det blir lagret. 
     except Exception as e: #Bruker messagebox til å vise en feilmelding hvis det oppstår et problem.
-        messagebox.showerror("Feilmelding", "Importering av brukere feilet! Pass på og tøm databasen før du importerer.")
+        messagebox.showerror("Feilmelding", "Importering av postnummer feilet! ")
 
 #Slett brukere, kobler meg til databasen.
 def FunkSlettBrukere():
@@ -57,37 +123,43 @@ def FunkSlettBrukere():
     try:
         c.execute('''DELETE FROM brukere''') #Sletter brukere
         conn.commit()
-    except sqlite3.Error as e:
+    except sqlite3.Error:
         messagebox.showerror("Feilmelding", "Sletting av brukere feilet!") #Messagebox hvis en feilmelding oppstår. 
     finally:
         conn.close()
 
-#Sletter den tomme databasen, bruker os modulen til å slette filen. Bruker også messagebox til å vise error hvis det ikke fungerer.
-#Noen ganger må man trykke 2 ganger på slett knappen for at den skal fungere. 
-def FunkSlettTomDatabase():
-    conn = sqlite3.connect('TomDatabase.db')
-    conn.close()
+#Slett postnummer, kobler meg til databasen.
+def FunkSlettPost():
+    conn = sqlite3.connect('Brukerdatabase.db') 
+    c = conn.cursor()
     try:
-        os.remove('TomDatabase.db')
-    except (sqlite3.Error, FileNotFoundError, PermissionError) as e:
-        messagebox.showerror("Feilmelding", "Sletting av tom database feilet! Pass på at den tomme databasen eksisterer ")
+        c.execute('''DELETE FROM postnummer''') #Sletter postnummer
+        conn.commit()
+    except sqlite3.Error:
+        messagebox.showerror("Feilmelding", "Sletting av postnummere feilet!") #Messagebox hvis en feilmelding oppstår. 
+    finally:
+        conn.close()
 
 #GUI
 def main():
 #Lager vinduet
     window = customtkinter.CTk()
     window.title('Administrer brukerdatabase')
-    window.geometry('250x300')
+    window.geometry('350x300')
 
 #Her er knappene, ganske selv forklart. Linker knappene til funksjonene jeg lagde tidligere. 
-    varLeggTil = customtkinter.CTkLabel(window, text="Legg til").pack(pady=5)
-    varLagDatabase = customtkinter.CTkButton(window, text="Lag en tom database", command=FunkLagDatabase).pack(pady=3)
-    varImporterCSV = customtkinter.CTkButton(window, text="Importer brukere i hoved databasen", command=FunkImporterCSV).pack(pady=3)
+    varLeggTil = customtkinter.CTkLabel(window, text="Legg til her", font=("Arial", 20)).pack(pady=5)
+    varLagDatabase = customtkinter.CTkButton(window, text="Lag en tom database", width=250, command=FunkLagDatabase).pack(pady=3)
+    varImporterCSV = customtkinter.CTkButton(window, text="Importer brukere i hoved databasen", width=250, command=FunkImporterCSV).pack(pady=3)
+    varImporterPostCSV = customtkinter.CTkButton(window, text="Importer postnummer i hoved databasen",  width=250,command=FunkImporterPostCSV).pack(pady=3)
 
-    varFjern = customtkinter.CTkLabel(window, text="Fjern").pack(pady=5)
-    varSlettBrukere = customtkinter.CTkButton(window, text="Slett brukere", fg_color="red", hover_color="maroon", command=FunkSlettBrukere).pack(pady=3)
-    varSlettTomDatabase = customtkinter.CTkButton(window, text="Slett den tomme database", fg_color="red", hover_color="maroon", command=FunkSlettTomDatabase).pack(pady=3)
+    varFjern = customtkinter.CTkLabel(window, text="Fjern her", font=("Arial", 20)).pack(pady=5)
+    varSlettBrukere = customtkinter.CTkButton(window, text="Slett brukere", fg_color="red", hover_color="maroon", width=250, command=FunkSlettBrukere).pack(pady=3)
+    varSlettPost = customtkinter.CTkButton(window, text="Slett postnummere", fg_color="red", hover_color="maroon", width=250, command=FunkSlettPost).pack(pady=3)
 
+
+    customtkinter.set_appearance_mode("dark")  #Setter fargene på vinduet til dark mode.
+    
     window.mainloop() #Kjører vinduet. 
 
 if __name__ == "__main__": #Bruker main til å kjøre programmet. 
